3cf97db405c7521a4119fe4919da6a2b8c2fc20b
Ufofa18 <danieludofa452@gmail.com>
2023-05-21 14:27:33 +0100

    update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import Banner from '../components/banner/Banner'
 import CoinsTable from '../components/banner/CoinsTable'
 import NewsFlash from '../components/Newslash'
 import ListCoins from '../components/ListCoins'
-import NewsHandler from '../components/newsHandler'
+import NewsHandler from '../components/NewsHandler'
 
 const Homepage = () => {
   return (
